fdbf3d84cdf213868c47dc2fbff8fe5703b6de18
Adam Michoń <adammplan@gmail.com>
2023-07-04 14:44:26 +0200

    material design forms

--- conectis-web/src/components/Calendar.tsx
+++ conectis-web/src/components/Calendar.tsx
@@ -4,10 +4,18 @@ import {
   CalendarProps,
   momentLocalizer,
 } from "react-big-calendar";
+
+import { Box, TextField, Button } from "@mui/material";
+
 import moment from "moment";
 import "moment/locale/pl";
-
+import { TimePicker } from "@mui/x-date-pickers/TimePicker";
 import "react-big-calendar/lib/css/react-big-calendar.css";
+
+import dayjs, { Dayjs } from "dayjs";
+import { DemoContainer } from "@mui/x-date-pickers/internals/demo";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 
 const localizer = momentLocalizer(moment);
 
@@ -21,10 +29,10 @@ export function Calendar() {
   const [notes, setNotes] = useState<Note[]>([]);
   const [selectedSlot, setSelectedSlot] = useState<any>(null);
   const [selectedDate, setSelectedDate] = useState<Date | null>(null);
-  const [noteText, setNoteText] = useState("");
-  const [startTime, setStartTime] = useState("");
-  const [endTime, setEndTime] = useState("");
+  const [selectedStartTime, setSelectedStartTime] = useState("");
+  const [selectedEndTime, setSelectedEndTime] = useState("");
   const [showNote, setShowNote] = useState(false);
+  const [noteText, setNoteText] = useState("");
 
   const handleDateSelect = (slotInfo: any) => {
     setSelectedSlot(slotInfo);
@@ -32,27 +40,42 @@ export function Calendar() {
     setShowNote(true);
   };
 
+  const handleStartTimeChange = (time: Dayjs | null) => {
+    if (time) {
+      const startTime = time.format("HH:mm");
+      setSelectedStartTime(startTime);
+    } else {
+      setSelectedStartTime("");
+    }
+  };
+
+  const handleEndTimeChange = (time: Dayjs | null) => {
+    if (time) {
+      const endTime = time.format("HH:mm");
+      setSelectedEndTime(endTime);
+    } else {
+      setSelectedEndTime("");
+    }
+  };
+
   const handleNoteChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setNoteText(event.target.value);
   };
 
-  const handleStartTimeChange = (
-    event: React.ChangeEvent<HTMLInputElement>
-  ) => {
-    setStartTime(event.target.value);
-  };
-
-  const handleEndTimeChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setEndTime(event.target.value);
-  };
-
   const handleAddNote = () => {
-    if (selectedDate && noteText && startTime && endTime) {
-      const startDateTime = moment(selectedDate).set(
-        "hour",
-        parseInt(startTime)
-      );
-      const endDateTime = moment(selectedDate).set("hour", parseInt(endTime));
+    if (
+      selectedDate &&
+      selectedStartTime &&
+      selectedEndTime &&
+      noteText &&
+      isEndTimeValid(selectedStartTime, selectedEndTime)
+    ) {
+      const startDateTime = moment(selectedDate)
+        .set("hour", parseInt(selectedStartTime.substring(0, 2)))
+        .set("minute", parseInt(selectedStartTime.substring(3, 5)));
+      const endDateTime = moment(selectedDate)
+        .set("hour", parseInt(selectedEndTime.substring(0, 2)))
+        .set("minute", parseInt(selectedEndTime.substring(3, 5)));
 
       const newNote: Note = {
         start: startDateTime.toDate(),
@@ -62,45 +85,84 @@ export function Calendar() {
       setNotes((prevNotes) => [...prevNotes, newNote]);
       setSelectedSlot(null);
       setSelectedDate(null);
+      setSelectedStartTime("");
+      setSelectedEndTime("");
       setNoteText("");
-      setStartTime("");
-      setEndTime("");
       setShowNote(false);
     }
   };
 
+  const isEndTimeValid = (startTime: string, endTime: string) => {
+    const startHour = parseInt(startTime.substring(0, 2));
+    const startMinute = parseInt(startTime.substring(3, 5));
+    const endHour = parseInt(endTime.substring(0, 2));
+    const endMinute = parseInt(endTime.substring(3, 5));
+
+    return (
+      endHour > startHour || (endHour === startHour && endMinute > startMinute)
+    );
+  };
+
+  const [value, setValue] = React.useState<Dayjs | null>(
+    dayjs("2022-04-17T15:30")
+  );
+
+  const handleNoteTextChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setNoteText(event.target.value);
+  };
+
   return (
     <div>
+      {showNote && (
+        <div className="note-pick">
+          <h1>Wybrany dzień: {moment(selectedDate).format("DD/MM/YYYY")}</h1>
+
+          <Box
+            component="form"
+            sx={{
+              "& .MuiTextField-root": { m: 1, width: "25ch" },
+            }}
+            noValidate
+            autoComplete="off"
+          >
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <TimePicker
+                label="Godzina rozpoczęcia"
+                value={
+                  selectedStartTime ? dayjs(selectedStartTime, "HH:mm") : null
+                }
+                onChange={handleStartTimeChange}
+              />
+              <TimePicker
+                label="Godzina zakończenia"
+                value={selectedEndTime ? dayjs(selectedEndTime, "HH:mm") : null}
+                onChange={handleEndTimeChange}
+              />
+            </LocalizationProvider>
+            <div>
+              <TextField
+                id="outlined-multiline-static"
+                label="Notatka"
+                multiline
+                rows={4}
+                value={noteText}
+                onChange={handleNoteTextChange}
+              />
+            </div>
+
+            <Button variant="contained" onClick={handleAddNote}>
+              Dodaj notatkę
+            </Button>
+          </Box>
+        </div>
+      )}
+
       <BigCalendar
         localizer={localizer}
         selectable
         events={notes}
         onSelectSlot={handleDateSelect}
       />
-      {showNote && (
-        <div>
-          <h1>Wybrany dzień: {moment(selectedDate).format("DD/MM/YYYY")}</h1>
-          <input
-            type="text"
-            value={noteText}
-            onChange={handleNoteChange}
-            placeholder="Wpisz notatkę..."
-          />
-          <label>
-            Godzina rozpoczęcia:
-            <input
-              type="time"
-              value={startTime}
-              onChange={handleStartTimeChange}
-            />
-          </label>
-          <label>
-            Godzina zakończenia:
-            <input type="time" value={endTime} onChange={handleEndTimeChange} />
-          </label>
-          <button onClick={handleAddNote}>Dodaj notatkę</button>
-        </div>
-      )}
     </div>
   );
 }
